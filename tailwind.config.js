0f0c90854fe74ebee09ac41a9c062470c678a489
iamakmal <m.akmalalavi@gmail.com>
2024-04-07 21:08:01 +0530

    initiated tailwind and developed navbar

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+      main:'#6B3CC9',
+      yellow:'#F7BF00',
+      black:'#171717',
+      white:'#FFFFFF',
+      red:'#FF7C46'
+    }},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "cupcake"],
+  },
+}
