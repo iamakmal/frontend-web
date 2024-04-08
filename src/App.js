d0a416db84151363cf1383b52f52349985c71cc6
iamakmal <m.akmalalavi@gmail.com>
2024-04-09 00:05:44 +0530

    implemented footer

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer";
 import FrequentlyAsked from "./components/FrequentlyAsked";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
@@ -11,6 +12,7 @@ function App() {
     <Hero/>
     <Services/>
     <FrequentlyAsked/>
+    <Footer/>
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <>
+<footer className="footer p-10 bg-main text-white px-[80px]">
+  <aside>
+  <img className="h-[25px] w-[238.89px] px-10 mb-[20px]" src="/Logo.png" alt="logo"/>
+    <p className="text-[16px] leading-[19.2px] font-normal">Your goal is our target. Not anything in between. We use<br/> online marketing platforms and tools to achieve single<br/> objective - your business results.</p>
+  </aside> 
+  <nav className="pl-[345px]">
+    <h6 className="text-white font-semibold text-[21px] leading-[25.41px]">Our Technologies</h6> 
+    <a className="link link-hover font-medium text-[14px] leading-[16.94px]">ReactJS</a>
+    <a className="link link-hover font-medium text-[14px] leading-[16.94px]">Gatsby</a>
+    <a className="link link-hover font-medium text-[14px] leading-[16.94px]">NextJS</a>
+    <a className="link link-hover font-medium text-[14px] leading-[16.94px]">NodeJS</a>
+  </nav> 
+  <nav className="pl-[128px]">
+    <h6 className="text-white font-semibold text-[21px] leading-[25.41px]">Our Services</h6> 
+    <a className="link link-hover font-medium text-[14px] leading-[16.94px]">Social Media Marketing</a>
+    <a className="link link-hover font-medium text-[14px] leading-[16.94px]">Web & Mobile App<br/> Development</a>
+    <a className="link link-hover font-medium text-[14px] leading-[16.94px]">Data & Analytics</a>
+  </nav> 
+</footer>
+<footer className="footer px-[405px] py-4 border-t bg-main text-white">
+  <nav className="place-self-center">
+  <p className="font-medium text-[14px] leading-[16.94px]">Privacy Policy | Terms & Conditions</p>
+  </nav>
+</footer>
+</>
+  )
+}
